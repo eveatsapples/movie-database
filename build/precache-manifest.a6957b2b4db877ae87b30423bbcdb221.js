7fc45d9e1632f9d620023d77b08c41ede175396b
Everett Penner <everettpenner@codebusiness.ca>
2019-01-28 19:26:14 -0600

    Initial build subtree commit

---
+++ build/precache-manifest.a6957b2b4db877ae87b30423bbcdb221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e5086afd0b55504057ce701fab5b2f8",
+    "url": "/static/media/logo.9e5086af.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1964d6db4921e3d7546b",
+    "url": "/static/js/main.1964d6db.chunk.js"
+  },
+  {
+    "revision": "f05bce0f9cc023510696",
+    "url": "/static/js/1.f05bce0f.chunk.js"
+  },
+  {
+    "revision": "1964d6db4921e3d7546b",
+    "url": "/static/css/main.a6589c65.chunk.css"
+  },
+  {
+    "revision": "c537ecbcdfd4986415419dd94cf5c28d",
+    "url": "/index.html"
+  }
+];
